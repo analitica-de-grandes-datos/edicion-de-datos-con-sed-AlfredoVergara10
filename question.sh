b50b0a95a1795928223b24196b99d1d5a118898a
AlfredoVergara10 <112211579+AlfredoVergara10@users.noreply.github.com>
2023-05-31 19:04:14 -0500

    Update question.sh

--- question.sh
+++ question.sh
@@ -41,3 +41,25 @@
 #
 #  >>> Escriba su codigo a partir de este punto <<<
 #
+# Convertir el año de "YY" a "YYYY".
+data=$(sed 's/\/\([0-9][0-9]\);/\/20\1;/g' data.csv |
+# Convertir el mes de "M" a "MM".
+sed 's/\/\([0-9]\)\//\/0\1\//g' |
+# Convertir el día de "D" a "DD".
+sed ':a;N;$!ba;s/\n\([0-9]\)\//\n0\1\//g' |
+# Convertir las fechas de "DD/MM/YY" a "YYYY-MM-DD".
+sed 's/\([0-9][0-9]\)\/\([0-9][0-9]\)\/\(20[0-9][0-9]\)/\3\-\2\-\1/g' |
+# Transformar todas las coincidiencias nulas entre ";" y ";" a ";\N;".
+sed 's/\(;N;\)\|\(;;\)\|\(;\\n;\)/;\\N;/g' |
+# Transformar todas las coincidencias nulas entre ";" y "\r" a "\N\r".
+sed 's/\(;\)\r\|\(;n\)\r/;\\N/g' |
+# Reemplazar coma decimal (",") por punto decimal (".").
+sed 's/\(,\)/./g' |
+# Reemplazar punto y comas (";") por comas (",").
+sed 's/\(;\)/,/g' |
+# Transformar todos los valores de la columna 2 a letras en mayúscula.
+sed 's/\([a-z]\)/\U&/g')
+echo "$data"
+
+
+
