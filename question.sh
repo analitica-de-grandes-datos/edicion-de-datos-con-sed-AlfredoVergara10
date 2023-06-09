e46b559136882e6a216f05a4a61b0f327bff55fe
AlfredoVergara10 <112211579+AlfredoVergara10@users.noreply.github.com>
2023-06-09 18:39:26 -0500

    Update question.sh

--- question.sh
+++ question.sh
@@ -43,11 +43,23 @@
 #
 
 
-
-sed -E 's/([0-9]{2})\/([0-9]{2})\/([0-9]{2})/20\3-\2-\1/g' $1 |
-sed -E 's/\\N/\N/g' |
-sed -E 's/;/,/g' |
-sed -E 's/,([0-9]+),([0-9]{1})/.\1\2/g'
-
-$ bash question.sh data.csv > output.csv
+# Convertir el año de "YY" a "YYYY".
+data=$(sed 's/\/\([0-9][0-9]\);/\/20\1;/g' data.csv 
+# Convertir el mes de "M" a "MM".
+sed 's/\/\([0-9]\)\//\/0\1\//g' |
+# Convertir el día de "D" a "DD".
+sed ':a;N;$!ba;s/\n\([0-9]\)\//\n0\1\//g' |
+# Convertir las fechas de "DD/MM/YY" a "YYYY-MM-DD".
+sed 's/\([0-9][0-9]\)\/\([0-9][0-9]\)\/\(20[0-9][0-9]\)/\3\-\2\-\1/g' |
+# Transformar todas las coincidiencias nulas entre ";" y ";" a ";\N;".
+sed 's/\(;N;\)\|\(;;\)\|\(;\\n;\)/;\\N;/g' |
+# Transformar todas las coincidencias nulas entre ";" y "\r" a "\N\r".
+sed 's/\(;\)\r\|\(;n\)\r/;\\N/g' |
+# Reemplazar coma decimal (",") por punto decimal (".").
+sed 's/\(,\)/./g' |
+# Reemplazar punto y comas (";") por comas (",").
+sed 's/\(;\)/,/g' |
+# Transformar todos los valores de la columna 2 a letras en mayúscula.
+sed 's/\([a-z]\)/\U&/g')
+echo "$data"
 
